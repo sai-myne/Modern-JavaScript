730f608be9795dfadd9ab1d46d2d7eaff9679ce8
sai-myne <ahlusir@gmail.com>
2020-06-30 15:33:52 +0800

    lesson 23. Else & Else If

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,18 +1,11 @@
-//  if statements
-// const age = 25;
-
-// if(age > 20){
-//     console.log('you are over 20 years old');
-// }
-
-// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
-
-// if(ninjas.length > 4){
-//     console.log("that's a lot of ninjas")
-// }
+//  else if statements
 
 const password = 'p@ssword';
 
-if(password.length >= 8){
+if(password.length >= 12){
+    console.log('that password is might strong');
+} else if(password.length >= 8){
     console.log('that password is long enough!');
+} else {
+    console.log('password is not long enough');
 }
