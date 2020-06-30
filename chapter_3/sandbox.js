9b6a58919c0d1b3600855227fafe9a98fc564dd6
sai-myne <ahlusir@gmail.com>
2020-06-30 15:48:37 +0800

    lesson 25. Logical NOT

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,11 +1,10 @@
-//  logical operators - OR || and AND &&
+//  logical NOT (!)
 
-const password = 'p@ss';
+let user = false;
 
-if(password.length >= 12 && password.includes('@')){
-    console.log('that password is might strong');
-} else if(password.length >= 8 || password.includes('@') && password.length >= 5){
-    console.log('that password is strong enough!');
-} else {
-    console.log('password is not strong enough');
+if(!user){
+    console.log('you must be logged in to continue');
 }
+
+console.log(!true);
+console.log(!false);
