1c87558eeb5260b33226f51d6a4bf427a1e3e835
sai-myne <ahlusir@gmail.com>
2020-06-30 16:05:23 +0800

    lesson 27. Switch Statements

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,17 +1,22 @@
-//  break and continue
+//  switch statements
+const grade = "50";
 
-const scores = [50, 25, 0, 30, 100, 20, 10];
-
-for(let i = 0; i < scores.length; i++){
-
-    if(scores[i] === 0){
-        continue;
-    }
-
-    console.log('your score: ', scores[i]);
-
-    if(scores[i] == 100){
-        console.log('congrats, you got the top score!');
-        break;
-    }
+switch (grade) {
+  case "A":
+    console.log("you got an A!");
+    break;
+  case "B":
+    console.log("you got an B!");
+    break;
+  case "C":
+    console.log("you got an C!");
+    break;
+  case "D":
+    console.log("you got an D!");
+    break;
+  case "E":
+    console.log("you got an E!");
+    break;
+  default:
+      console.log('not a valid grade');
 }
