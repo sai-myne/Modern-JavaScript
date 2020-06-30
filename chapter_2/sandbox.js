9ac0011b1272f8738a6debd293edfd755715061c
sai-myne <ahlusir@gmail.com>
2020-06-30 14:33:03 +0800

    lesson 14. Null & Undefined

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,22 +1,3 @@
-let ninjas = ['shaun', 'ryu', 'chun-li'];
+let age = null;
 
-// ninjas[1] = 'ken';
-// console.log(ninjas[1]);
-
-// let ages = [20,25,30,35];
-// console.log(ages[2]);
-
-// let random = ['shaun', 'crystal', 30, 20];
-// console.log(random);
-
-// console.log(ninjas.length);
-
-// array methods
-
-// let result = ninjas.join('-');
-// let result = ninjas.indexOf('chun-li');
-// let result = ninjas.concat(['ken', 'crystal']);
-let result = ninjas.push('ken');
-result = ninjas.pop();
-
-console.log(result);
+console.log(age, age + 3, `the age is ${age}`);
