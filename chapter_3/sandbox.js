9a55e6d6ab40d22b987798d5fc9fe59d82d0acf1
sai-myne <ahlusir@gmail.com>
2020-06-30 15:19:27 +0800

    lesson 20. While Loops

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,15 +1,14 @@
-// for loops
+// while loops
+const name = ['shaun', 'mario', 'luigi'];
+// let i = 0;
 
-// for(let i = 0; i < 5; i++){
+// while(i < 5){
 //     console.log('in loop: ', i);
+//     i++;
 // };
 
-// console.log('loop finished');
-
-const names = ['shaun', 'mario', 'luigi'];
-
-for(let i = 0; i < names.length; i++){
-    // console.log(names[i]);
-    let html = `<div>${names[i]}</div>`;
-    console.log(html)
+let i = 0;
+while(i < name.length){
+    console.log(name[i])
+    i++
 }
