586992870f29411d502752b9983ac37d64059a7f
sai-myne <ahlusir@gmail.com>
2020-07-01 10:57:36 +0800

    lesson 42. Math Object

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,36 +1,18 @@
-// object literals
+// Math object
 
-// const blogs = [
-//     { title: 'why mac & cheese rules', likes: 30},
-//     { title: '10 things to make with marmite', likes: 50}
-// ];
+console.log(Math)
+console.log(Math.PI);
 
-// console.log(blogs);
+console.log(Math.E);
 
-let user = {
-    name: 'crystal',
-    age: 30,
-    email: 'crystal@thenetninja.co.uk',
-    location: 'berlin',
-    blogs: [
-        { title: 'why mac & cheese rules', likes: 30},
-        { title: '10 things to make with marmite', likes: 50}
-    ],
-    login(){
-        console.log('the user logged in');
-    },
-    logout(){
-        console.log('the user logged out');
-    },
-    logBlogs(){
-        // console.log(this.blogs);
-        console.log('this user has written the following blogs:');
-        this.blogs.forEach(blog => {
-            console.log(blog.title, blog.likes);
-        })
-    }
-};
+const area = 7.7;
+console.log(Math.round(area))
+console.log(Math.floor(area))
+console.log(Math.ceil(area))
+console.log(Math.trunc(area))
 
-user.logBlogs();
-console.log(this);
+// random numbers
 
+const random = Math.random();
+console.log(random);
+console.log(Math.round(random * 100));
