afd50c99a1c2f77a9fb15c95871738bdcff336d5
sai-myne <ahlusir@gmail.com>
2020-07-01 13:31:03 +0800

    lesson 46. The Query Selector

---
+++ chapter_6/sandbox.js
@@ -0,0 +1,9 @@
+// const para = document.querySelector('body > h1');
+
+
+// console.log(para);
+
+const paras = document.querySelectorAll('p');
+const errors = document.querySelectorAll('.error');
+
+console.log(errors);
