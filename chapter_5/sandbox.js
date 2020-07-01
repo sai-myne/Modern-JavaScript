f3589896e423b16a3dfb3b6e8a4abf7240885b99
sai-myne <ahlusir@gmail.com>
2020-07-01 10:46:44 +0800

    lesson 40. 'this' Keyword

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -6,20 +6,21 @@ let user = {
     email: 'crystal@thenetninja.co.uk',
     location: 'berlin',
     blogs: ['why mac & cheese rules', '10 things to make with marmite'],
-    login: function(){
+    login(){
         console.log('the user logged in');
     },
-    logout: function(){
+    logout(){
         console.log('the user logged out');
     },
-    logBlogs: function(){
-        
+    logBlogs(){
+        // console.log(this.blogs);
+        console.log('this user has written the following blogs:');
+        this.blogs.forEach(blog => {
+            console.log(blog);
+        })
     }
 };
 
-user.login();
-user.logout();
+user.logBlogs();
+console.log(this);
 
-const name = 'mario';
-
-console.log(name.toUpperCase())
