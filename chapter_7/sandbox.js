a7de6916ff17c6c9c933cdd1fec7f49b60b017d7
sai-myne <ahlusir@gmail.com>
2020-07-01 16:03:07 +0800

    lesson 62. Basic Form Validation

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -1,23 +1,19 @@
 const form = document.querySelector('.signup-form');
-// const username = document.querySelector('#username');
+const feedback = document.querySelector('.feedback')
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    // console.log(username.value);
-    console.log(form.username.value);
+
+    // validation
+    const username = form.username.value;
+    const usernamePattern = /^[a-zA-Z]{6,12}$/;
+
+    if(usernamePattern.test(username)){
+        // feedback good info
+        feedback.textContent = 'That username is valid!'
+    } else {
+        // feedback help info
+        feedback.textContent = 'username must contain letters only & be between 6 & 12 characters long'
+    }
+
 })
-
-// testing RegEx
-const username = '1323sshaun23123';
-const pattern = /^[a-z]{6,}$/;
-
-// let result = pattern.test(username);
-
-// if(result){
-//     console.log('regex test passed :) ');
-// } else {
-//     console.log('regex test failed :(');
-// }
-
-let result = username.search(pattern)
-console.log(result);
