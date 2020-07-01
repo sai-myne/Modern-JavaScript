1fa077f432d7bd5111dee65f53325721f0ad90fa
sai-myne <ahlusir@gmail.com>
2020-07-01 10:02:14 +0800

    lesson 33. Arrow Functions

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,16 +1,31 @@
-// returing values
+// practise arrow functions
 
-// const speak = function(name = "luigi", time = 'night'){
-//     console.log(`good ${time} ${name}`);
+// const greet = function(){
+//     return 'hello, world';
 // };
 
-const calcArea = function(radius){
-    return 3.14 * radius**2
+// const greet = () => 'hello, world';
+
+// const result = greet();
+// console.log(result)
+
+// const bill = function (products, tax) {
+//     let total = 0;
+//     for(let i=0; i< products.length; i++){
+//         total += products[i] + products [i] * tax;
+//     }
+//     return total;
+// }
+
+const bill = (products, tax) => {
+    let total = 0;
+    for(let i=0; i< products.length; i++){
+        total += products[i] + products [i] * tax;
+    }
+    return total;
 }
 
-const area = calcArea(5);
-console.log(area);
-
+console.log(bill([10,15,30], 0.2))
 
 
 
