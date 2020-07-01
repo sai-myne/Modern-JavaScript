98dd0069ce9bd15b3c9a883cba3fafb915cb4a10
sai-myne <ahlusir@gmail.com>
2020-07-01 13:38:27 +0800

    lesson 47. Other Ways to Query the DOM

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,9 +1,13 @@
-// const para = document.querySelector('body > h1');
+// get an element by ID
+// const title = document.getElementById("page-title");
+// console.log(title);
 
+// get elements by their class name
+// const errors = document.getElementsByClassName('error')
+// console.log(errors);
+// console.log(errors[0]);
 
-// console.log(para);
-
-const paras = document.querySelectorAll('p');
-const errors = document.querySelectorAll('.error');
-
-console.log(errors);
+// get elements by their tag name
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[0])
