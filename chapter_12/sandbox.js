12a74b01d117349a7e273268260bef79b1f2ed8b
sai-myne <ahlusir@gmail.com>
2020-07-02 15:41:07 +0800

    lesson 96. The Fetch API

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,31 +1,10 @@
-const getTodos = (resource) => {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
+// fetch api
 
-    request.addEventListener("readystatechange", () => {
-      // console.log(request, request.readyState);
-      if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText);
-        resolve(data);
-      } else if (request.readyState === 4) {
-        reject('error getting resource');
-      }
-    });
-
-    request.open("GET", resource);
-    request.send();
-  });
-};
-
-getTodos('todos/luigi.json').then((data) => {
-    console.log('promise 1 resolved:', data);
-    return getTodos('todos/mario.json');
+fetch('todos/luigi.json').then((response) => {
+    console.log('resolved', response);
+    return response.json();
 }).then((data) => {
-    console.log('promise 2 resolved:', data);
-    return getTodos('todos/shaun.json')
-}).then((data) => {
-    console.log('promise 3 resolved:', data);
-}).catch((err) => {
-    console.log('promise rejected:', err)
-})
-
+    console.log(data);
+}).catch(() => {
+    console.log('rejected', err);
+});
