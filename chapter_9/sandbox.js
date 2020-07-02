e7ba52949194069abe03e888ea7c626e317730ac
sai-myne <ahlusir@gmail.com>
2020-07-02 10:44:16 +0800

    lesson 72. Map Method

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -1,21 +1,24 @@
-const scores = [10, 30, 15, 25, 50, 40, 5];
+// map method
+const prices = [20, 10, 30, 25, 15, 40, 80, 5];
 
-// const filteredScores = scores.filter((score) => {
-//     return score > 20;
-// });
+// const salePrices = prices.map(price => price / 2);
 
-// console.log(filteredScores);
+// console.log(salePrices)
 
-
-const users = [
-    {name: 'shaun', premiun: true},
-    {name: 'yoshi', premiun: false},
-    {name: 'mario', premiun: false},
-    {name: 'chun-li', premiun: true},
+const products = [
+    {name: 'gold star', price: 20},
+    {name: 'mushroom', price: 40},
+    {name: 'green shells', price: 30},
+    {name: 'banana skin', price: 10},
+    {name: 'red shells', price: 50}
 ];
 
-const premiumUsers = users.filter(user => {
-    return user.premiun;
+const saleProducts = products.map((product) => {
+    if(product.price > 30){
+        return {name: product.name, price: product.price / 2}
+    } else {
+        return product;
+    }
 })
+console.log(saleProducts)
 
-console.log(premiumUsers);
