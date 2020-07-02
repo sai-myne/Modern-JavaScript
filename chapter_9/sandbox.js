56c6e3bbce8deb2bdbb85e7fa2cabe3e882bc9c8
sai-myne <ahlusir@gmail.com>
2020-07-02 12:09:26 +0800

    lesson 75. Sort Method

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -1,12 +1,39 @@
-// find method
-const scores = [10, 5, 0, 40, 30, 10, 90, 70];
+// example 1 - sorting strings
+const names = ['mario', 'shaun', 'chun-li', 'yoshi', 'luigi'];
 
-const firstHighScore = scores.find((score) => {
-    return score > 50;
-})
+// names.sort();
+// names.reverse();
+// console.log(names);
 
-console.log(firstHighScore);
+// example 2 - sorting numbers
+const scores = [10, 50, 20, 5, 35, 70, 45];
 
+// names.sort();
+// scores.reverse();
+// console.log(scores);
 
+scores.sort((a,b) => b-a)
+console.log(scores)
 
+// example 3 - sorting objects
+const players = [
+    {name: 'mario', score: 20},
+    {name: 'luigi', score: 10},
+    {name: 'chun-li', score: 50},
+    {name: 'yoshi', score: 30},
+    {name: 'shaun', score: 70}
+  ];
 
+// players.sort((a, b) => {
+//     if(a.score > b.score){
+//         return -1;
+//     } else if (b.score > a.score){
+//         return 1;
+//     } else {
+//         return 0;
+//     }
+// });
+
+players.sort((a, b) => b.score - a.score)
+
+console.log(players)
