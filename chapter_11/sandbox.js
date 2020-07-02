6c7a4d245fd0e3738fdd149b42010ed8108fe970
sai-myne <ahlusir@gmail.com>
2020-07-02 13:55:31 +0800

    lesson 82. Dates & Times in JavaScript

---
+++ chapter_11/sandbox.js
@@ -0,0 +1,22 @@
+// dates & times
+const now = new Date();
+
+console.log(now);
+console.log(typeof now);
+
+// year, months, day, times
+console.log('getFullYear:', now.getFullYear());
+console.log('getMonth:', now.getMonth()+1);
+console.log('getDate:', now.getDate());
+console.log('getDay:', now.getDay());
+console.log('getHours:', now.getHours());
+console.log('getMinutes:', now.getMinutes());
+console.log('getSeconds:', now.getSeconds());
+
+// timestamps
+console.log('timestamp: ', now.getTime())
+
+// date string
+console.log(now.toDateString());
+console.log(now.toTimeString());
+console.log(now.toLocaleString());
