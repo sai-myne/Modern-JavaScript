a0fcdb8c37c6745791241d31fad081f565b8b6af
sai-myne <ahlusir@gmail.com>
2020-07-02 14:02:56 +0800

    lesson 83. Timestamps & Comparisons

--- chapter_11/sandbox.js
+++ chapter_11/sandbox.js
@@ -1,22 +1,21 @@
-// dates & times
+// timestamp
+
+const before = new Date('Feburary 1 2019 7:30:59');
 const now = new Date();
 
-console.log(now);
-console.log(typeof now);
+// console.log(now.getTime(), before.getTime());
 
-// year, months, day, times
-console.log('getFullYear:', now.getFullYear());
-console.log('getMonth:', now.getMonth()+1);
-console.log('getDate:', now.getDate());
-console.log('getDay:', now.getDay());
-console.log('getHours:', now.getHours());
-console.log('getMinutes:', now.getMinutes());
-console.log('getSeconds:', now.getSeconds());
+const diff = now.getTime() - before.getTime();
+console.log(diff);
 
-// timestamps
-console.log('timestamp: ', now.getTime())
+const mins = Math.round(diff / 1000 / 60);
+const hours = Math.round(mins / 60);
+const days = Math.round(hours / 24);
 
-// date string
-console.log(now.toDateString());
-console.log(now.toTimeString());
-console.log(now.toLocaleString());
+console.log(mins, hours, days);
+
+console.log(`the blog was witten ${days} ago`);
+
+// converting timestamps into date objects
+const timestamp = 44692226301;
+console.log(new Date(timestamp));
