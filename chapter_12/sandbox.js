a0c6df086e3c5beebf16c846d207cec1dd99afb4
sai-myne <ahlusir@gmail.com>
2020-07-02 15:28:26 +0800

    lesson 94. Promise Basics

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,29 +1,47 @@
-const getTodos = (resource, callback) => {
-  const request = new XMLHttpRequest();
+const getTodos = (resource) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
 
-  request.addEventListener("readystatechange", () => {
-    // console.log(request, request.readyState);
-    if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText)
-      callback(undefined, data);
-    } else if (request.readyState === 4) {
-      callback('could not fetch data', undefined);
-    }
+    request.addEventListener("readystatechange", () => {
+      // console.log(request, request.readyState);
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject('error getting resource');
+      }
+    });
+
+    request.open("GET", resource);
+    request.send();
   });
-
-  request.open("GET", resource);
-  request.send();
 };
 
+getTodos('todos/luigis.json').then((data) => {
+    console.log('promise resolved:', data)
+}).catch((err) => {
+    console.log('promise rejected:', err)
+})
 
-getTodos('todos/luigi.json', (err, data) => {
-    console.log(data);
-    getTodos('todos/mario.json', (err, data) => {
-        console.log(data);
-        getTodos('todos/shaun.json', (err, data) => {
-            console.log(data);
-        });
-;    });
-    
-});
+// promise example
 
+// const getSomething = () => {
+
+//     return new Promise((resolve, reject) => {
+//         // fetch something
+//         resolve('some data');
+//         // reject('some error');
+//     });
+// };
+
+// getSomething().then((data) => {
+//     console.log(data)
+// }, (err) => {
+//     console.log(err);
+// });
+
+// getSomething().then((data) => {
+//     console.log(data);
+// }).catch(err => {
+//     console.log(err);
+// })
