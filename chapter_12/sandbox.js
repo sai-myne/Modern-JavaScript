314b6dfb82e70688f8cbb42c1c36e33ce4d8930d
sai-myne <ahlusir@gmail.com>
2020-07-02 15:09:57 +0800

    lesson 92. JSON Data

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -4,18 +4,17 @@ const getTodos = (callback) => {
   request.addEventListener("readystatechange", () => {
     // console.log(request, request.readyState);
     if (request.readyState === 4 && request.status === 200) {
-      callback(undefined, request.responseText);
+        const data = JSON.parse(request.responseText)
+      callback(undefined, data);
     } else if (request.readyState === 4) {
       callback('could not fetch data', undefined);
     }
   });
 
-  request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+  request.open("GET", "todo.json");
   request.send();
 };
 
-console.log(1);
-console.log(2);
 
 getTodos((err, data) => {
     console.log('callback fired');
@@ -25,7 +24,4 @@ getTodos((err, data) => {
         console.log(data)
     }
 });
-
-console.log(3);
-console.log(4);
 
