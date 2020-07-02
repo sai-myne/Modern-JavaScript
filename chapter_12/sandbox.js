cc9374021d866ad50ecfafd067d76a1473e5939d
sai-myne <ahlusir@gmail.com>
2020-07-02 15:57:02 +0800

    lesson 98. Throwing & Catching errors

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -3,22 +3,18 @@
 const getTodos = async () => {
 
     const response = await fetch('todos/luigi.json');
+
+    if(response.status !== 200){
+        throw new Error('cannot fetch the data');
+    }
+
     const data = await response.json();
     
     return data;
 
 };
-console.log(1)
-console.log(2)
+
 getTodos()
-    .then(data => console.log('resolved:', data));
-console.log(3)
-console.log(4)
-// fetch('todos/luigi.json').then((response) => {
-//     console.log('resolved', response);
-//     return response.json();
-// }).then((data) => {
-//     console.log(data);
-// }).catch(() => {
-//     console.log('rejected', err);
-// });
+    .then(data => console.log('resolved:', data))
+    .catch(err => console.log('rejected:', err.message));
+
