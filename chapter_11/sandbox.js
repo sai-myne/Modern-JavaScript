66c807f1605f0a21800f96d02d1c7b8d494dc7da
sai-myne <ahlusir@gmail.com>
2020-07-02 14:20:03 +0800

    lesson 85. Date-fns Library

--- chapter_11/sandbox.js
+++ chapter_11/sandbox.js
@@ -1,21 +1,15 @@
-const clock = document.querySelector('.clock');
+const now = new Date();
 
-const tick = () => {
+// console.log(dateFns.isToday(now));
 
-  const now = new Date();
+// formatting options
+console.log(dateFns.format(now, 'YYYY'));
+console.log(dateFns.format(now, 'MMM'));
+console.log(dateFns.format(now, 'dddd'));
+console.log(dateFns.format(now, 'Do'));
+console.log(dateFns.format(now, 'dddd Do MMMM YYYY'));
 
-  const h = now.getHours();
-  const m = now.getMinutes();
-  const s = now.getSeconds();
+// comparing dates
+const before = new Date('Feburary 1 2019 12:00:00');
 
-  const html = `
-    <span>${h}</span> :
-    <span>${m}</span> :
-    <span>${s}</span>
-  `;
-
-  clock.innerHTML = html;
-
-};
-
-setInterval(tick, 1000);
+console.log(dateFns.distanceInWords(now, before, {addSuffix: true}));
