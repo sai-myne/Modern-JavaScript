b27960461b469da26369f36af6062938acaaeb3b
sai-myne <ahlusir@gmail.com>
2020-07-03 15:34:57 +0800

    lesson 132. Deleting Documents

--- chapter_16/sandbox.js
+++ chapter_16/sandbox.js
@@ -1,12 +1,13 @@
 const list = document.querySelector('ul');
 const form = document.querySelector('form');
 
-const addRecipe = (recipe) => {
+const addRecipe = (recipe, id) => {
     let time = recipe.created_at.toDate();
     let html = `
-        <li>
+        <li data-id="${id}">
             <div>${recipe.title}</div>
             <div class="text-muted">${time}</div>
+            <button class="btn btn-danger btn-sm my-2">delete</button>
         </li>
     `;
     list.innerHTML += html;
@@ -15,7 +16,7 @@ const addRecipe = (recipe) => {
 db.collection('recipes').get().then((snapshot) => {
     // when we have the data
     snapshot.docs.forEach(doc => {
-        addRecipe(doc.data());
+        addRecipe(doc.data(), doc.id);
     })
 }).catch((err) => {
     console.log(err);
@@ -35,3 +36,14 @@ form.addEventListener('submit', e => {
         console.log(err);
     });
 });
+
+// deleting data
+list.addEventListener('click', e => {
+    // console.log(e);
+    if(e.target.tagName === 'BUTTON'){
+        const id = e.target.parentElement.getAttribute('data-id');
+        db.collection('recipes').doc(id).delete().then(() => {
+            console.log('recipe deleted');
+        });
+    }
+})
