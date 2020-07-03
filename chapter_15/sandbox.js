d40a041e74d79bdf9de498c0474fd092e3429be7
sai-myne <ahlusir@gmail.com>
2020-07-03 14:02:58 +0800

    lesson 122. Constructors(under the hood)

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -1,47 +1,32 @@
-class User {
-    constructor(username, email){
-        // set up properties
-        this.username = username;
-        this.email = email;
-        this.score = 0;
-    }
-    login(){
-        console.log(`${this.username} just logged in`);
-        return this;
-    }
-    logout(){
-        console.log(`${this.username} just logged out`);
-        return this;
-    }
-    incScore(){
-        this.score += 1;
-        console.log(`${this.username} has a score of ${this.score}`);
-        return this;
-    }
+// constructor functions
+
+function User(username, email){
+    this.username = username;
+    this.email = email;
+    this.login = function(){
+        console.log(`${this.username} has logged in`)
+    };
 }
 
-class Admin extends User{
-    constructor(username, email, title){
-        super(username, email);
-        this.title = title;
-    }
-    deleteUser(user){
-        users = users.filter((u) => u.username !== user.username)
-    }
-}
+// class User {
+//     constructor(username, email){
+//         // set up properties
+//         this.username = username;
+//         this.email = email;
+//     }
+    
+// }
+
+
 
 const userOne = new User('mario', 'mario@thenetninja.co.uk');
 const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
-const userThree = new Admin('shaun', 'shaun@thenetninja.co.uk', 'black-belt-ninja');
 
-console.log(userThree);
+console.log(userOne, userTwo);
+userOne.login();
 
-userOne.login().incScore().incScore().logout();
-
-let users = [userOne, userTwo, userThree];
-console.log(users);
-
-userThree.deleteUser(userTwo);
-console.log(users);
-
+// the 'new' keyword
+// 1 - it creates a new empty object {}
+// 2 - it binds the value of 'this' to the new empty object
+// 3 - it calls the constructor function to 'build' the object
 
