17c087b8e0c30e9a9ba89c2c91377da0cf0e94d0
sai-myne <ahlusir@gmail.com>
2020-07-03 14:14:40 +0800

    lesson 123. Prototype Model

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -3,30 +3,21 @@
 function User(username, email){
     this.username = username;
     this.email = email;
-    this.login = function(){
-        console.log(`${this.username} has logged in`)
-    };
 }
 
-// class User {
-//     constructor(username, email){
-//         // set up properties
-//         this.username = username;
-//         this.email = email;
-//     }
-    
-// }
+User.prototype.login = function(){
+    console.log(`${this.username} has logged in`);
+    return this;
+}
 
-
+User.prototype.logout = function(){
+    console.log(`${this.username} has logged out`);
+    return this;
+}
 
 const userOne = new User('mario', 'mario@thenetninja.co.uk');
 const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
 
 console.log(userOne, userTwo);
-userOne.login();
-
-// the 'new' keyword
-// 1 - it creates a new empty object {}
-// 2 - it binds the value of 'this' to the new empty object
-// 3 - it calls the constructor function to 'build' the object
+userOne.login().logout();
 
