247b4242842dec6b036dc6a7a4d529759a855006
sai-myne <ahlusir@gmail.com>
2020-07-03 09:54:29 +0800

    lesson 103. Get Weather API Call

--- Weather_app/scripts/forecast.js
+++ Weather_app/scripts/forecast.js
@@ -1,6 +1,19 @@
 const key = 'BFT0oKMAjmBeDcpCaF3UQQuLDWQJ2dRE'
 
+// get weather information
+const getWeather = async (id) => {
 
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+
+// get city information
 const getCity = async (city) => {
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -13,5 +26,11 @@ const getCity = async (city) => {
 };
 
 getCity('manchester')
-    .then(data => console.log(data))
+    .then(data => {
+        return getWeather(data.Key);
+    })
+    .then(data => {
+        console.log(data);
+    })
     .catch(err => console.log(err));
+
