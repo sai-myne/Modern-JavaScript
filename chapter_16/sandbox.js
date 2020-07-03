ccc27b3216135d7c9efdc4c41e1a911c0e4058b2
sai-myne <ahlusir@gmail.com>
2020-07-03 15:24:27 +0800

    lesson 131. Saving Documents

--- chapter_16/sandbox.js
+++ chapter_16/sandbox.js
@@ -1,4 +1,5 @@
 const list = document.querySelector('ul');
+const form = document.querySelector('form');
 
 const addRecipe = (recipe) => {
     let time = recipe.created_at.toDate();
@@ -18,4 +19,19 @@ db.collection('recipes').get().then((snapshot) => {
     })
 }).catch((err) => {
     console.log(err);
-})
+});
+
+// add documents
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const now = new Date();
+    const recipe = {
+        title: form.recipe.value,
+        created_at: firebase.firestore.Timestamp.fromDate(now)
+    };
+    db.collection('recipes').add(recipe).then(() => {
+        console.log('recipe added');
+    }).catch(err => {
+        console.log(err);
+    });
+});
