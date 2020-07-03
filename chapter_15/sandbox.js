998d760553faf7506e3b8706689cf965e4014ed6
sai-myne <ahlusir@gmail.com>
2020-07-03 14:24:50 +0800

    lesson 124. Prototype Inheritance

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -8,16 +8,29 @@ function User(username, email){
 User.prototype.login = function(){
     console.log(`${this.username} has logged in`);
     return this;
-}
+};
 
 User.prototype.logout = function(){
     console.log(`${this.username} has logged out`);
     return this;
+};
+
+function Admin(username, email, title){
+    User.call(this, username, email);
+    this.title = title;
 }
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function(){
+    // delete a user
+    
+};
 
 const userOne = new User('mario', 'mario@thenetninja.co.uk');
 const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
+const userThree = new Admin('shaun', 'shaun@thenetninja.co.uk', 'black-belt-ninja');
 
-console.log(userOne, userTwo);
+console.log(userOne, userTwo, userThree);
 userOne.login().logout();
 
