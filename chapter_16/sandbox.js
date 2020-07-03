803347c5b2dbd40a24f7b75d10183c37464dd284
sai-myne <ahlusir@gmail.com>
2020-07-03 15:16:15 +0800

    lesson 130. Getting Collections

--- chapter_16/sandbox.js
+++ chapter_16/sandbox.js
@@ -1,1 +1,21 @@
+const list = document.querySelector('ul');
 
+const addRecipe = (recipe) => {
+    let time = recipe.created_at.toDate();
+    let html = `
+        <li>
+            <div>${recipe.title}</div>
+            <div class="text-muted">${time}</div>
+        </li>
+    `;
+    list.innerHTML += html;
+}
+
+db.collection('recipes').get().then((snapshot) => {
+    // when we have the data
+    snapshot.docs.forEach(doc => {
+        addRecipe(doc.data());
+    })
+}).catch((err) => {
+    console.log(err);
+})
