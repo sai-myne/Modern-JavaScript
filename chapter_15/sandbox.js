e1c91c5d875c580bd6bb102725c3ffd962484117
sai-myne <ahlusir@gmail.com>
2020-07-03 14:28:13 +0800

    lesson 125. Built-in Objects

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -24,13 +24,12 @@ Admin.prototype = Object.create(User.prototype);
 
 Admin.prototype.deleteUser = function(){
     // delete a user
-    
+
 };
 
 const userOne = new User('mario', 'mario@thenetninja.co.uk');
 const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
 const userThree = new Admin('shaun', 'shaun@thenetninja.co.uk', 'black-belt-ninja');
 
-console.log(userOne, userTwo, userThree);
-userOne.login().logout();
+
 
