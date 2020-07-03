cc41996688c1b30978d7ce1179c31c8b963d2b81
sai-myne <ahlusir@gmail.com>
2020-07-03 13:53:39 +0800

    lesson 120. Class Inheritance(subclasses)

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -20,15 +20,25 @@ class User {
     }
 }
 
+class Admin extends User{
+    deleteUser(user){
+        users = users.filter((u) => u.username !== user.username)
+    }
+}
+
 const userOne = new User('mario', 'mario@thenetninja.co.uk');
 const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
+const userThree = new Admin('shaun', 'shaun@thenetninja.co.uk');
 
-console.log(userOne, userTwo);
+console.log(userOne, userTwo, userThree);
 
 userOne.login().incScore().incScore().logout();
 
+let users = [userOne, userTwo, userThree];
+console.log(users);
 
-// the 'new' keyword
-// 1 - it creates a new empty object {}
-// 2 = it binds the value of 'this' to the new empty object
-// 3 - it calls the constructor function to 'build' the object
+userThree.deleteUser(userTwo);
+console.log(users);
+
+userOne.deleteUser(userThree);
+
