5c7533f3ad3bb90940f6027df419aaca8bc96b16
sai-myne <ahlusir@gmail.com>
2020-07-03 15:48:19 +0800

    lesson 133. Real-time Listeners

--- chapter_16/sandbox.js
+++ chapter_16/sandbox.js
@@ -13,13 +13,25 @@ const addRecipe = (recipe, id) => {
     list.innerHTML += html;
 }
 
-db.collection('recipes').get().then((snapshot) => {
-    // when we have the data
-    snapshot.docs.forEach(doc => {
-        addRecipe(doc.data(), doc.id);
-    })
-}).catch((err) => {
-    console.log(err);
+const deleteRecipe = (id) => {
+    const recipes = document.querySelectorAll('li');
+    recipes.forEach(recipe => {
+        if(recipe.getAttribute('data-id') === id){
+            recipe.remove();
+        }
+    });
+}
+
+// get documents
+db.collection('recipes').onSnapshot(snapshot => {
+    snapshot.docChanges().forEach(change => {
+        const doc = change.doc;
+        if(change.type === 'added'){
+            addRecipe(doc.data(), doc.id);
+        } else if (change.type === 'removed'){
+            deleteRecipe(doc.id);
+        }
+    });
 });
 
 // add documents
