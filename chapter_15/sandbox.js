a356be15eea32b7f2b50edd642d3656bbd7a9568
sai-myne <ahlusir@gmail.com>
2020-07-03 13:44:57 +0800

    lesson 119. Class Methods & Method Chaining

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -1,9 +1,22 @@
-
 class User {
     constructor(username, email){
         // set up properties
         this.username = username;
         this.email = email;
+        this.score = 0;
+    }
+    login(){
+        console.log(`${this.username} just logged in`);
+        return this;
+    }
+    logout(){
+        console.log(`${this.username} just logged out`);
+        return this;
+    }
+    incScore(){
+        this.score += 1;
+        console.log(`${this.username} has a score of ${this.score}`);
+        return this;
     }
 }
 
@@ -11,6 +24,9 @@ const userOne = new User('mario', 'mario@thenetninja.co.uk');
 const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
 
 console.log(userOne, userTwo);
+
+userOne.login().incScore().incScore().logout();
+
 
 // the 'new' keyword
 // 1 - it creates a new empty object {}
